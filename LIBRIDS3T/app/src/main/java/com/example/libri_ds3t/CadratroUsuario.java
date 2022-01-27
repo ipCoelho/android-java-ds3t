1e3e26c3bdb82c221df65ab4a1668c3bf2904fa4
suporte <pcoelho.business@gmail.com>
2022-01-27 13:37:22 -0300

    initial commit

---
+++ LIBRIDS3T/app/src/main/java/com/example/libri_ds3t/CadratroUsuario.java
@@ -0,0 +1,14 @@
+package com.example.libri_ds3t;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadratroUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadratro_usuario);
+    }
+}
